c93a99054c0595d9df889e96ad1cfe84cef68c74
Alexander Gray <alexander@ultamatum.dev>
2021-11-10 15:43:14 +0000

    Fixed it, for some reason expanded widget broke ti

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -14,64 +14,60 @@ class MainScreen extends StatelessWidget {
         child: ListView(
           physics: const BouncingScrollPhysics(),
           children: [
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.only(top: 95),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Text(
-                      'Alexander Gray',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.headline3!.copyWith(
-                            color: Theme.of(context).primaryColor,
-                            fontSize: 48,
-                            fontWeight: FontWeight.bold,
-                          ),
-                    ),
-                    const SizedBox(height: 56),
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          'Programming for Games, Web & Mobile',
-                          textAlign: TextAlign.center,
-                          style:
-                              Theme.of(context).textTheme.headline1!.copyWith(
-                                    fontSize: 41,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+            Container(
+              padding: const EdgeInsets.only(top: 95),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Text(
+                    'Alexander Gray',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.headline3!.copyWith(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
                         ),
-                        const SizedBox(height: 0),
-                        SvgPicture.asset(
-                          'assets/Swoosh.svg',
-                          semanticsLabel: 'Swoosh',
-                          width: 819.5,
-                          height: 36.19,
-                        ),
-                        const SizedBox(height: 0),
-                        Text(
-                          'I design and code anything you need, and love doing it.',
-                          textAlign: TextAlign.center,
-                          style:
-                              Theme.of(context).textTheme.subtitle1!.copyWith(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.normal,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 56),
-                    const GetInTouchButton(),
-                    SvgPicture.asset(
-                      'assets/HeroImage.svg',
-                      semanticsLabel: 'HeroImage',
-                      width: 818,
-                      height: 514,
-                    ),
-                  ],
-                ),
+                  ),
+                  const SizedBox(height: 56),
+                  Column(
+                    children: <Widget>[
+                      Text(
+                        'Programming for Games, Web & Mobile',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.headline1!.copyWith(
+                              fontSize: 41,
+                              fontWeight: FontWeight.bold,
+                            ),
+                      ),
+                      const SizedBox(height: 0),
+                      SvgPicture.asset(
+                        'assets/Swoosh.svg',
+                        semanticsLabel: 'Swoosh',
+                        width: 819.5,
+                        height: 36.19,
+                      ),
+                      const SizedBox(height: 0),
+                      Text(
+                        'I design and code anything you need, and love doing it.',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.subtitle1!.copyWith(
+                              fontSize: 22,
+                              fontWeight: FontWeight.normal,
+                            ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 56),
+                  const GetInTouchButton(),
+                  SvgPicture.asset(
+                    'assets/HeroImage.svg',
+                    semanticsLabel: 'HeroImage',
+                    width: 818,
+                    height: 514,
+                  ),
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
