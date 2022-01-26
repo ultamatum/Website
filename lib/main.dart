cae747e4a79998ebaf609a3be796db944ce98c1e
ultamatum <alexanderg0502@gmail.com>
2022-01-26 17:37:09 +0000

    Removed .env currenly

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:website/models/theme.dart';
 import 'package:website/screens/main_screen.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 
 Future<void> main() async {
-  await dotenv.load();
-
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
